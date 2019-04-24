0c450491a296b923e7cd385cf8026ec02f5aa179
Thiago Oliveira <thiago4x70@gmail.com>
2019-04-24 13:19:15 -0300

    Add a file input directive, does the same as the bootstrap version but it only uses angular

--- Auto_relatorio_coverage/app/controllers/main.controller.js
+++ Auto_relatorio_coverage/app/controllers/main.controller.js
@@ -2,8 +2,26 @@
     'use strict';
 
     angular
-        .module('Relatorio', ['ScrollTop', 'ScrollBottom', 'FileInput'])
-        .controller('mainCtrl', ['$scope', function ($scope){
+        .module('Relatorio', ['ScrollTop', 'ScrollBottom', 'FileInput', 'FileReader', 'TestDirective'])
+        .controller('mainCtrl', ['$scope', 'fileReader', function ($scope, fileReader){
+            $scope.getFile = function () {
+                $scope.progress = 0;
+                fileReader.readAsDataUrl($scope.file, $scope)
+                              .then(function(result) {
+                                  $scope.imageSrc = result;
+                              });
+            };
+         
+            $scope.$on("fileProgress", function(e, progress) {
+                $scope.progress = progress.loaded / progress.total;
+            });
+
+            $scope.readFile = function () {            
+                fileReader.readAsDataUrl($scope.file, $scope)
+                          .then(function(result) {
+                                $scope.imageSrc = result;
+                            });
+            };
         }]);
 
 })(window.angular);
---
+++ Auto_relatorio_coverage/app/directives/file-input.directive.js
@@ -0,0 +1,29 @@
+(function() {
+    'use strict';
+
+    angular
+        .module('TestDirective', [])
+        .directive('fileInput', fileInput);
+
+    function fileInput($parse) {
+        return {
+            restrict: "EA",
+            template: "<input type='file' />",
+            replace: true,          
+            link: function (scope, element, attrs) {
+                var modelGet = $parse(attrs.fileInput);
+                var modelSet = modelGet.assign;
+                var onChange = $parse(attrs.onChange);
+     
+                var updateModel = function () {
+                    scope.$apply(function () {
+                        modelSet(scope, element[0].files[0]);
+                        onChange(scope);
+                    });                    
+                };
+                 
+                element.bind('change', updateModel);
+            }
+        };
+    };
+})();

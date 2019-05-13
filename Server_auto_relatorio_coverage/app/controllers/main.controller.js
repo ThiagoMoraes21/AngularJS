8dbc3c3fc7a0e55077e43e7719d69d218884e919
Thiago Oliveira <thiago4x70@gmail.com>
2019-05-13 15:51:01 -0300

    Made some changes in the controller and in the index doc

--- Server_auto_relatorio_coverage/app/controllers/main.controller.js
+++ Server_auto_relatorio_coverage/app/controllers/main.controller.js
@@ -3,13 +3,13 @@ angular
     .module('Relatorio', ['ScrollTop', 'QuickViewCardModule'])
     .controller('mainCtrl', ['$scope', function ($scope){
         $scope.cards  = [
-            {"name":"KONICA 1", "serie": "ASD930202003", "model": "BH-2393", "pv": "789.321", "av" : "5,06"},
-            {"name":"KONICA 2", "serie": "ASDGA2452653", "model": "BH-2393", "pv": "123.321", "av" : "3,06"},
-            {"name":"KONICA 3", "serie": "JGFJFG434534", "model": "ER-3452", "pv": "789.321", "av" : "4,56"},
-            {"name":"KONICA 4", "serie": "3453453DG343", "model": "ASD-345", "pv": "545.212", "av" : "3,96"},
-            {"name":"KONICA 5", "serie": "SDFG43453535", "model": "DF-4555", "pv": "789.321", "av" : "7,06"},
-            {"name":"KONICA 6", "serie": "3453453DG343", "model": "ASD-345", "pv": "545.212", "av" : "4,92"},
-            {"name":"KONICA 7", "serie": "SDFG43453535", "model": "DF-4555", "pv": "789.321", "av" : "6,06"}
+            {"name":"KONICA 1", "serie": "ASD930202003", "model": "BH-2393", "pv": "789.321", "av": "5,06", "data": "01/01/2019"},
+            {"name":"KONICA 2", "serie": "ASDGA2452653", "model": "BH-2393", "pv": "123.321", "av": "3,06", "data": "23/02/2019"},
+            {"name":"KONICA 3", "serie": "JGFJFG434534", "model": "ER-3452", "pv": "789.321", "av": "4,56", "data": "12/12/2018"},
+            {"name":"KONICA 4", "serie": "3453453DG343", "model": "ASD-345", "pv": "545.212", "av": "3,96", "data": "23/03/2019"},
+            {"name":"KONICA 5", "serie": "SDFG43453535", "model": "DF-4555", "pv": "789.321", "av": "7,06", "data": "06/03/2019"},
+            {"name":"KONICA 6", "serie": "3453453DG343", "model": "ASD-345", "pv": "545.212", "av": "4,92", "data": "13/05/2018"},
+            {"name":"KONICA 7", "serie": "SDFG43453535", "model": "DF-4555", "pv": "789.321", "av": "6,06", "data": "24/05/2019"}
         ];
 
         // calcula total de pv das impressoras
